6972a7c090231bd66e8d8d64fdc5b63f12381817
Joao7498 <Jbraatz400@gmail.com>
2026-05-27 15:09:42 -0300

    logo

--- vite-project/src/pages/Login/Login.jsx
+++ vite-project/src/pages/Login/Login.jsx
@@ -1,44 +1,54 @@
 import { Link } from "react-router-dom";
 import { ROUTES } from "../../constants/routes.js";
+import logo from "../../imagens/logo.png";
 import "./Login.css";
 
 export default function Login() {
   return (
     <div className="login-root">
-    <div className="container">
-      <div className="card">
+      <div className="container">
+        <div className="card">
 
-        <div className="logo">
-          
-          <h1>AgroGestor</h1>
-          <p>Gestão Rural Simplificada</p>
-        </div>
+          <div className="logo">
 
-        <form onSubmit={(e) => e.preventDefault()}>
-          <label>Email ou Usuário</label>
-          <input type="text" placeholder="Digite seu email" />
+        
 
-          <label>Senha</label>
-          <input type="password" placeholder="Digite sua senha" />
+            <img
+              src={logo}
+              alt="Logo AgroGestor"
+              className="logo-img"
+            />
+            
 
-          <a href="#" className="forgot">Esqueci minha senha</a>
+            <p>Gestão Rural Simplificada</p>
+          </div>
 
-          <Link to={ROUTES.HOME} className="btn-primary">
-            Entrar
+          <form onSubmit={(e) => e.preventDefault()}>
+            <label>Email ou Usuário</label>
+            <input type="text" placeholder="Digite seu email" />
+
+            <label>Senha</label>
+            <input type="password" placeholder="Digite sua senha" />
+
+            <a href="#" className="forgot">Esqueci minha senha</a>
+
+            <Link to={ROUTES.HOME} className="btn-primary">
+              Entrar
+            </Link>
+          </form>
+
+          <p className="no-account">Não tem conta?</p>
+
+          <Link to={ROUTES.CADASTRO_PRODUTOR} className="btn-outline">
+            Cadastro de Produtor
           </Link>
-        </form>
 
-        <p className="no-account">Não tem conta?</p>
+          <Link to={ROUTES.CADASTRO_EMPRESA} className="btn-outline">
+            Cadastro de Empresa/Cooperativa
+          </Link>
 
-        <Link to={ROUTES.CADASTRO_PRODUTOR} className="btn-outline">
-          Cadastro de Produtor
-        </Link>
-        <Link to={ROUTES.CADASTRO_EMPRESA} className="btn-outline">
-          Cadastro de Empresa/Cooperativa
-        </Link>
-
+        </div>
       </div>
-    </div>
     </div>
   );
 }
