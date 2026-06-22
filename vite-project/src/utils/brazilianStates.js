b310c243207b3e7349f7b919cc20d789b1b97fd0
b0ggo <joaobatistaabc132@gmail.com>
2026-06-22 18:57:09 -0300

    adicionar modulo estados brasileiros

---
+++ vite-project/src/utils/brazilianStates.js
@@ -0,0 +1,41 @@
+export const ESTADOS_BRASILEIROS = [
+  { code: 'AC', name: 'Acre' },
+  { code: 'AL', name: 'Alagoas' },
+  { code: 'AP', name: 'Amapá' },
+  { code: 'AM', name: 'Amazonas' },
+  { code: 'BA', name: 'Bahia' },
+  { code: 'CE', name: 'Ceará' },
+  { code: 'DF', name: 'Distrito Federal' },
+  { code: 'ES', name: 'Espírito Santo' },
+  { code: 'GO', name: 'Goiás' },
+  { code: 'MA', name: 'Maranhão' },
+  { code: 'MT', name: 'Mato Grosso' },
+  { code: 'MS', name: 'Mato Grosso do Sul' },
+  { code: 'MG', name: 'Minas Gerais' },
+  { code: 'PA', name: 'Pará' },
+  { code: 'PB', name: 'Paraíba' },
+  { code: 'PR', name: 'Paraná' },
+  { code: 'PE', name: 'Pernambuco' },
+  { code: 'PI', name: 'Piauí' },
+  { code: 'RJ', name: 'Rio de Janeiro' },
+  { code: 'RN', name: 'Rio Grande do Norte' },
+  { code: 'RS', name: 'Rio Grande do Sul' },
+  { code: 'RO', name: 'Rondônia' },
+  { code: 'RR', name: 'Roraima' },
+  { code: 'SC', name: 'Santa Catarina' },
+  { code: 'SP', name: 'São Paulo' },
+  { code: 'SE', name: 'Sergipe' },
+  { code: 'TO', name: 'Tocantins' },
+];
+
+export const obterEstadosPorCodigo = () => ESTADOS_BRASILEIROS;
+
+export const obterEstadoPorSigla = (codigo) => {
+  if (!codigo) return null;
+  return ESTADOS_BRASILEIROS.find(estado => estado.code === codigo.toUpperCase());
+};
+
+export const ehSiglaEstadoValida = (codigo) => {
+  if (!codigo || typeof codigo !== 'string') return false;
+  return ESTADOS_BRASILEIROS.some(estado => estado.code === codigo.toUpperCase());
+};
